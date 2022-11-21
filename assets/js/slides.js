167101e97a46a8946e06325714e3462ab4cabef8
Franklin da Silva Batista <frsbatist@gmail.com>
2022-11-21 13:58:35 -0300

    21/11/2022   13:59

---
+++ assets/js/slides.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const slideWrapper = document.querySelector('[data-slide="wrapper"]')
+const slideList = document.querySelector('[data-slide="list"]')
+const navPreviousButton = document.querySelector('[data-slide="nav-previous-button"]')
+const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
+const controlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
+const slideItem =document.querySelectorAll('[data-slide="item"]')
+const controlButtons = document.querySelectorAll('[data-slide="control-button"]')
+
